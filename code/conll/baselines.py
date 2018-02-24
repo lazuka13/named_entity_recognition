6a9e8c19e49991ce3d96db5e3fc9f37978916023
Zufar Latypov <lazuka13@gmail.com>
2018-02-24 20:29:37 +0300

    Добавил нахождение оптимального веса признаков

--- code/conll/baselines.py
+++ code/conll/baselines.py
@@ -51,9 +51,9 @@ def run_baselines(mode):
         },
         'gb': {
             'clf': 'XGBClassifier',
-            'parameters': [{"booster": ['gbtree', 'gblinear'],
-                            'learning_rate': [0.01, 0.05, 0.1, 0.2, 0.3, 0.4],
-                            'max_depth': [3, 6, 10, 15],
+            'parameters': [{"booster": ['gbtree'],
+                            'learning_rate': [0.05, 0.2, 0.3, 0.4],
+                            'max_depth': [10, 14, 17],
                             'colsample_bytree': [0.5, 0.8, 1],
                             'colsample_bylevel': [0.5, 0.8, 1]}]
         },
---
+++ code/conll/checks/best_features_weight.py
@@ -0,0 +1,69 @@
+from sklearn.model_selection import cross_val_score
+import numpy as np
+import matplotlib.pyplot as plt
+
+import logging
+
+import features
+import reader
+
+from classifiers.token_classifier import TokenClassifier
+
+logger = logging.getLogger('logger')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s',
+                              datefmt='%H:%M:%S')
+
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+
+def run_best_features_weight():
+    possible_weights = [i / 100 for i in range(80, 100, 1)]
+    results = []
+
+    for possible_weight in possible_weights:
+        dataset = reader.DataReader('../dataset', fileids='eng.train.txt',
+                                    columntypes=('words', 'pos', 'chunk', 'ne'))
+        gen = features.Generator(columntypes=('words', 'pos', 'chunk'), context_len=2, language='en',
+                                 rare_count=5, min_weight=possible_weight, rewrite=True, history=True)
+
+        y = [el[1] for el in dataset.get_ne()]
+        x = gen.fit_generate(dataset.get_tags(tags=['words', 'pos', 'chunk']), y,
+                             "../prepared_data/conll_trainset.npz")
+
+        x_sent, y_sent = [], []
+        index = 0
+        for sent in dataset.sents():
+            length = len(sent)
+            if length == 0:
+                continue
+            x_sent.append(x[index:index + length])
+            y_sent.append(y[index:index + length])
+            index += length
+        x_docs, y_docs = [], []
+        index = 0
+        for doc in dataset.docs():
+            length = len(doc)
+            if length == 0:
+                continue
+            x_docs.append(x_sent[index:index + length])
+            y_docs.append(y_sent[index:index + length])
+            index += length
+
+        clf = TokenClassifier(cls='XGBClassifier')
+        clf.set_params(booster='gbtree', colsample_bylevel=0.5, colsample_bytree=0.5, learning_rate=0.3, max_depth=14)
+        results.append(np.mean(cross_val_score(clf, x_docs, y_docs, n_jobs=-1)))
+    print(possible_weights)
+    print(results)
+    plt.plot(possible_weights, results)
+    plt.savefig('./graph.png')
+
+
+if __name__ == '__main__':
+    run_best_features_weight()
